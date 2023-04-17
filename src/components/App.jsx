807e7fa0f5d80a5f6ff56180fa56544749185b08
Kirill Voronin <voroninkv09@gmail.com>
2023-04-17 15:17:53 +0300

    change App.jsx from from state to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Section } from './Feedbacks/Section/Section';
 import { FeedbackOptions } from './Feedbacks/FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Feedbacks/Statistics/Statistics';
 import css from 'components/Feedbacks/FeedbackOptions/Feedback.module.css';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  styledButtonAfterClick = event => {
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackStatuses = { good, neutral, bad };
+
+  const styledButtonAfterClick = event => {
     event.target.classList.add(`${css.active}`);
     setTimeout(() => {
       event.target.classList.remove(`${css.active}`);
     }, 300);
   };
-  onButtonClick = event => {
+  const onButtonClick = event => {
     const { name } = event.currentTarget;
-    this.styledButtonAfterClick(event);
-    return this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    styledButtonAfterClick(event);
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        break;
+    }
   };
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-  countPositiveFeedbackPercentage() {
-    return !this.countTotalFeedback()
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return !countTotalFeedback()
       ? 0
-      : Math.ceil((this.state.good / this.countTotalFeedback()) * 100);
-  }
+      : Math.ceil((good / countTotalFeedback()) * 100);
+  };
 
-  render() {
-    return (
-      <>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onButtonClick}
-          ></FeedbackOptions>
-        </Section>
-        <Section>
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          ></Statistics>
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={Object.keys(feedbackStatuses)}
+          onLeaveFeedback={onButtonClick}
+        ></FeedbackOptions>
+      </Section>
+      <Section>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        ></Statistics>
+      </Section>
+    </>
+  );
 }
